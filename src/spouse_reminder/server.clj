4430603698720fadee12035cb0ea04a315b504cf
Dale-Harrison <dailofernandez@gmail.com>
2012-03-14 20:37:36 +0000

    hard coded url in redirect to test

--- src/spouse_reminder/server.clj
+++ src/spouse_reminder/server.clj
@@ -16,7 +16,7 @@
 (defn require-https
   [handler]
   (fn [request]
-    (println (:headers request))))
+    (println ("x-forwarded-proto" (:headers request)))))
 
 (server/add-middleware require-https)
 
